d2a288292b1dbe7fe25daeebeb144ca4fddef6f7
O_o <vanhellsing1996@mail.ru>
2017-08-24 01:29:16 +0300

    Completed version

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import {CollectorFieldListComponent,CongratulationComponent} from "./data-collector/field-list.component";
+import {ResponseListComponent} from "./admin-cab/responses/response-list.component";
+import {FieldEditorComponent} from "./admin-cab/fields/field-editor.component";
+import {FieldListComponent} from "./admin-cab/fields/field-list.component";
+import {LoginComponent} from "./admin-cab/loginer/login.component";
+import {RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+
+//----------------------------------------------------------------------------------------------------------------------
+@NgModule({
+  imports : [RouterModule.forRoot([
+    {path:''                  ,component: CollectorFieldListComponent},
+    {path:'congratulation'    ,component: CongratulationComponent},
+    {path:'responses'         ,component: ResponseListComponent},
+    {path:'fields/editor/:id' ,component: FieldEditorComponent},
+    {path:'fields'            ,component: FieldListComponent},
+    {path:'login'             ,component: LoginComponent}
+  ])],
+  exports : [RouterModule]
+})
+export class AppRoutingModule{}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
